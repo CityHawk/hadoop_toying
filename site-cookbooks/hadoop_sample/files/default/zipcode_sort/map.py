dfdd5a8ecd80fb62d8485f2c0b32d63aea14d6ee
Evgeny Suchkov <suchkov@google.com>
2013-06-04 18:59:14 -0400

    Fixed map script

--- site-cookbooks/hadoop_sample/files/default/zipcode_sort/map.py
+++ site-cookbooks/hadoop_sample/files/default/zipcode_sort/map.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 for lineIn in sys.stdin:
-       zip = lineIn[0:5]
-#       Note: Key is defined here
-       print zip + '\t' + lineIn
+    zip = lineIn[1:6]
+    #       Note: Key is defined here
+    if not zip=='zip",':
+        sys.stdout.write(zip + '\t' + lineIn)
