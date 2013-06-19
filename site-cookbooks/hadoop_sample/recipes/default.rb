d1056f1f858b99151ca74508a31091340ec76f5c
Eugene Suchkov <suchkov@google.com>
2013-06-19 17:01:28 -0400

    Added less complicated example method

--- site-cookbooks/hadoop_sample/recipes/default.rb
+++ site-cookbooks/hadoop_sample/recipes/default.rb
@@ -9,41 +9,21 @@
 
 # The example is taken from http://www.analyticalway.com/?p=124
 
-directory "/var/lib/hdfs/zipcode_sort" do
-    recursive true
-    owner "hdfs"
-end
 
-cookbook_file "/var/lib/hdfs/zipcode_sort/map.py" do
-    source "zipcode/map.py"
-    owner "hdfs"
-end
-
-cookbook_file "/var/lib/hdfs/zipcode_sort/reducer.py" do
-    source "zipcode/reducer.py"
-    owner "hdfs"
-end
-
-package "unzip"
-
-bash "extract zbp07detail.zip" do
-    cwd "/var/lib/hdfs/zipcode_sort"
+bash "hput data" do
     user "hdfs"
     code <<-EOB
-    unzip zbp07detail.zip
     hadoop fs -mkdir -p /user/mydir
-    hadoop fs -put zbp07detail.txt /user/mydir/
+    hadoop fs -put /tmp/pg76.txt /user/mydir/
     EOB
     action :nothing
 end
 
-remote_file "/var/lib/hdfs/zipcode_sort/zbp07detail.zip" do
-    source "http://www2.census.gov/econ2007/CBP_CSV/zbp07detail.zip"
+remote_file "/tmp/pg76.txt" do
+    source "http://www.gutenberg.org/cache/epub/76/pg76.txt"
     owner "hdfs"
-    notifies :run, "bash[extract zbp07detail.zip]"
+    notifies :run, "bash[hput data]"
 end
 
-
-
 # you can launch all that stuff with
-# hadoop jar /usr/lib/hadoop-0.20-mapreduce/contrib/streaming/hadoop-streaming-2.0.0-mr1-cdh4.2.1.jar -input /user/mydir/zbp07detail.txt -output /user/mydir/out -mapper ./map.py -reducer ./reducer.py -file ./map.py -file ./reducer.py
+# hadoop jar /usr/lib/hadoop-0.20-mapreduce/hadoop-examples.jar wordcount /user/mydir/pg76.txt HF.out
