024e1d2e1e90e3ff79c591ef84223c52a2a51229
Evgeny Suchkov <suchkov@google.com>
2013-06-20 21:11:51 -0400

    savegame

--- foodcourt/lib/foodcourt/worker.rb
+++ foodcourt/lib/foodcourt/worker.rb
@@ -1,3 +1,31 @@
+require "foodcourt/node"
+require 'digital_ocean'
+
 class Worker
+
+    attr_accessor :steps, :env_id
+
+    def initialize blueprint_file
+         @blueprint = YAML.load_file("blueprint.yml")
+         @nodes = blueprint["nodes"]
+         @dc = DigitalOcean::API.new :client_id => @config_data['authentication']['client_key'], :api_key => @config_data['authentication']['api_key'],
+         
+    end
+
+    def go
+        @nodes.each do |node, data|
+            threads << Thread.new do
+                n = Node.new :env_id => @env_id,
+                    :id => node,
+                    :domain => config[:domain],
+                    :cloud => @dc,
+                    :size => data["size"],
+                    :image => data["size"],
+                    :roles => data["roles"],
+                    :ssh_keys => config["ssh_keys"]
+            end
+        end
+
+    end
 
 end
