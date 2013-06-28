ec4858bc792f4bfa2ed6e215b491cec49d662ea6
Eugene Suchkov <suchkov@google.com>
2013-06-28 16:36:15 -0400

    Use rsync instead of bootstrapStandby

--- site-cookbooks/myhadoop/recipes/namenode.rb
+++ site-cookbooks/myhadoop/recipes/namenode.rb
@@ -24,7 +24,7 @@ if namenodes.length == 2
                    :journalnodes => journalnodes,
                    :zookeepers => zookeepers
     end
-    if zookeepers.length > 0
+    if zookeepers && zookeepers.length > 0
         # fencing keys
         directory "/var/lib/hadoop-hdfs/.ssh" do
             owner "hdfs"
@@ -77,6 +77,31 @@ if namenodes.length == 2
             :zookeepers => zookeepers
         end
     end
+    if node["hadoop"]["namenode"]["primary"]
+        bash "format hdfs node" do
+            user "hdfs"
+            code "hdfs namenode -format"
+            creates "/var/lib/hadoop-hdfs/cache/hdfs/dfs/name/current/VERSION"
+            notifies :start, "service[hadoop-hdfs-namenode]", :immediately
+        end
+    else
+        if zookeepers && zookeepers.length > 2
+            primarynn = search(:node,"chef_environment:#{node.chef_environment} AND hadoop_namenode_primary:true").first
+            bash "rsync standby" do
+                user "hdfs"
+                code "rsync -avz -e ssh hdfs@#{primarynn["fqdn"]}:/var/lib/hadoop-hdfs/cache/* /var/lib/hadoop-hdfs/cache/"
+                creates "/var/lib/hadoop-hdfs/cache/hdfs/dfs/name/current/VERSION"
+                notifies :start, "service[hadoop-hdfs-namenode]", :immediately
+            end
+        else
+            bash "bootstrap standby" do
+                user "hdfs"
+                code "hdfs namenode -bootstrapStandby"
+                creates "/var/lib/hadoop-hdfs/cache/hdfs/dfs/name/current/VERSION"
+                notifies :start, "service[hadoop-hdfs-namenode]", :immediately
+            end
+        end
+    end
 else
     # in case of 3 or more nns it will fallback to the 1
     template "/etc/hadoop/conf/core-site.xml" do
@@ -85,27 +110,12 @@ else
         variables :namenode => namenodes.first,
                    :ha => false
     end
-end
 
-if node["hadoop"]["namenode"]["primary"]
     bash "format hdfs node" do
         user "hdfs"
         code "hdfs namenode -format"
         creates "/var/lib/hadoop-hdfs/cache/hdfs/dfs/name/current/VERSION"
         notifies :start, "service[hadoop-hdfs-namenode]", :immediately
     end
-    # if namenodes.length == 2
-    #     bash "init shared edits" do
-    #         user "hdfs"
-    #         code "hdfs namenode -initializeSharedEdits"
-    #         ignore_failure true
-    #     end
-    # end
-else
-    # bash "bootstrap standby" do
-    #     user "hdfs"
-    #     code "hdfs namenode -bootstrapStandby"
-    #     creates "/var/lib/hadoop-hdfs/cache/hdfs/dfs/name/current/VERSION"
-    #     notifies :start, "service[hadoop-hdfs-namenode]", :immediately
-    # end
 end
+
